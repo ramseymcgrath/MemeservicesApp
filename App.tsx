ca8ba5de18d60e9b297c7e67c711e097cd3a4d5c
Ramsey McGrath <ramsey.mcgrath@paddypowerbetfair.com>
2020-06-18 18:38:20 -0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Memeservices Demo App</Text>
+      <Text>The Home of Gourmet Memes!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
